8a0496d8cf214ccee6587591ffbd61d006e125a3
Alex Karepin <akarepin@gmail.com>
2020-05-06 17:33:06 +0300

    Initial commit - Task-008

---
+++ app/src/main/java/com/karepin/homework_008/MainActivity.java
@@ -0,0 +1,102 @@
+package com.karepin.homework_008;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import java.util.GregorianCalendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mChooseStartDate;
+    private Button mChooseEndDate;
+    private CalendarView mStartDateCalendar;
+    private CalendarView mEndtDateCalendar;
+
+    private long mStartDate;
+    private String mStartDateTxt;
+    private long mEndDate;
+    private String mEndDateTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        inputButtonOk();
+    }
+
+    private void initViews() {
+        mChooseStartDate = findViewById(R.id.chooseStartDate);
+        mChooseEndDate = findViewById(R.id.chooseEndDate);
+        mStartDateCalendar = findViewById(R.id.startDateCalendar);
+        mEndtDateCalendar = findViewById(R.id.endtDateCalendar);
+
+        // скроем календари при запуске приложения
+        mStartDateCalendar.setVisibility(View.GONE);
+        mEndtDateCalendar.setVisibility(View.GONE);
+
+        // метод показывающий календарь для выбора старта задачи
+        mChooseStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mStartDateCalendar.setVisibility(View.VISIBLE);
+                mEndtDateCalendar.setVisibility(View.GONE);
+            }
+        });
+        // метод показывающий календарь для выбора окончания задачи
+        mChooseEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mEndtDateCalendar.setVisibility(View.VISIBLE);
+                mStartDateCalendar.setVisibility(View.GONE);
+            }
+        });
+        // метод выбора даты старта задачи
+        mStartDateCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                mStartDateTxt = i2 + "-" + (i1 + 1) + "-" + i;
+                mChooseStartDate.setText("Дата-время старта задачи: " + mStartDateTxt);
+                GregorianCalendar gregorianCalendar = new GregorianCalendar();
+                gregorianCalendar.set(i, i1, i2);
+                mStartDate = gregorianCalendar.getTimeInMillis();
+                calendarView.setVisibility(View.GONE);
+            }
+        });
+        // метод выбора даты окончания задачи
+        mEndtDateCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                mEndDateTxt = i2 + "-" + (i1 + 1) + "-" + i;
+                mChooseEndDate.setText("Дата-время окончания задачи: " + mEndDateTxt);
+                GregorianCalendar gregorianCalendar = new GregorianCalendar();
+                gregorianCalendar.set(i, i1, i2);
+                mEndDate = gregorianCalendar.getTimeInMillis();
+                calendarView.setVisibility(View.GONE);
+            }
+        });
+    }
+        // метод провекри на правильность ввода данных (дата начала не должна быть меньше даты окончания)
+        // и вывода введенных данных на экран
+    private void inputButtonOk() {
+        Button mBtnOK = findViewById(R.id.btnOK);
+        mBtnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mStartDate > mEndDate) {
+                    Toast.makeText(MainActivity.this, "Ошибка", Toast.LENGTH_LONG).show();
+                    mChooseStartDate.setText("Дата-время старта задачи:");
+                    mChooseEndDate.setText("Дата-время окончания задачи:");
+                } else {
+                    Toast.makeText(MainActivity.this, "старт: " + mStartDateTxt + " окончаниe: " + mEndDateTxt, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
